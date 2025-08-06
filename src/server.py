1f772b17065bf44e1c4bd907cd9f3e04c6eff8a3
Reza Rahemtola <reza.rahemtola@epitech.eu>
2025-08-07 00:48:08 +0200

    fix: Remove useless fastapi deps

--- src/server.py
+++ src/server.py
@@ -1,6 +1,3 @@
-import asyncio
-from contextlib import asynccontextmanager
-
 from fastapi import FastAPI
 from starlette.middleware.cors import CORSMiddleware
 
@@ -10,21 +7,7 @@ from src.proxy import router as proxy_router
 
 keys_manager = KeysManager()
 
-
-async def run_jobs():
-    while True:
-        await keys_manager.refresh_keys()
-        await asyncio.sleep(30)
-
-
-@asynccontextmanager
-async def lifespan(_app: FastAPI):
-    print("Starting server...")
-    asyncio.create_task(run_jobs())
-    yield
-
-
-app = FastAPI(title="LibertAI backend service", lifespan=lifespan)
+app = FastAPI(title="LibertAI backend service")
 
 
 app.add_middleware(
